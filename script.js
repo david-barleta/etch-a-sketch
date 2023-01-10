24ce1b9e2c538406cfa8b4c038add135f7cf3d7f
David <davidjoshua285@gmail.com>
2023-01-10 23:44:21 +0800

    Add sketch area to the web page
    - Add header
    - Add code that creates a grid
    - Add code that colors a square when the mouse hovers over the square
    while the mouse is pressed

---
+++ script.js
@@ -0,0 +1,39 @@
+const grid = document.querySelector(".grid");
+let sketchEnabled = false;
+
+
+function color(square) {
+  if (sketchEnabled) {
+    square.style.backgroundColor = "black";
+  } else {
+    return;
+  }
+}
+
+function createGrid(gridSize) {
+  for (g = 0; g < gridSize; g++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (i = 0; i < gridSize; i++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      square.setAttribute(`style`, `height: ${500/gridSize}px; width: ${500/gridSize}px`);
+      row.appendChild(square);
+      square.addEventListener('mouseover', function() {color(square)});
+    }
+    grid.appendChild(row);
+  }
+}
+
+grid.addEventListener('mousedown', () => {
+  console.log("mouse down");
+  sketchEnabled = true;
+})
+
+grid.addEventListener('mouseup', () => {
+  console.log("mouse up");
+  sketchEnabled = false;
+})
+
+
+createGrid(10);
