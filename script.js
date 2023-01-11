fd5c6530081e60f42d21db0aa4f4e1c325e070d9
David <davidjoshua285@gmail.com>
2023-01-12 01:05:09 +0800

    Fix coloring bug
    - Fix bug wherein the first square hovered over by the cursor when the
    mouse button is pressed is not colored

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const grid = document.querySelector(".grid");
 const colors = document.querySelectorAll(".color");
 const gridSizes = document.querySelectorAll(".size");
@@ -5,14 +6,23 @@ const clear = document.querySelector("#clear");
 const eraser = document.querySelector("#eraser");
 
 let sketchEnabled = false;
+let shadingEnabled = true;
 let currentColor = "#1C1C1E"
 
 function color(square) {
   if (sketchEnabled) {
-    square.style.backgroundColor = currentColor;
+    if (shadingEnabled === false) {
+      square.style.backgroundColor = currentColor;
+    } else if (shadingEnabled === true) {
+      square.style.backgroundColor = currentColor;
+    }
   } else {
     return;
   }
+}
+
+function colorFirst(square) {
+  square.style.backgroundColor = currentColor;
 }
 
 function clearGrid() {
@@ -32,17 +42,18 @@ function createGrid(gridSize) {
       square.classList.add('square');
       square.setAttribute(`style`, `height: ${500/gridSize}px; width: ${500/gridSize}px`);
       row.appendChild(square);
+      square.addEventListener('mousedown', function() {colorFirst(square)});
       square.addEventListener('mouseover', function() {color(square)});
     }
     grid.appendChild(row);
   }
 }
 
-grid.addEventListener('mousedown', () => {
+body.addEventListener('mousedown', () => {
   sketchEnabled = true;
 })
 
-grid.addEventListener('mouseup', () => {
+body.addEventListener('mouseup', () => {
   sketchEnabled = false;
 })
 
